b864cad12b05c44cf869b1449e4c5e3b4d5a473f
Noman Ali <noman710@protonmail.com>
2022-01-10 14:04:30 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
+
 
 function App() {
+
+  const [posts, setPosts] = useState([])
+
+  // call API
+  const fetchPost = async () => {
+    const response = await axios("http://ctp-zip-api.herokuapp.com/zip/10010")
+    console.log(response.data)
+    setPosts(response.data)
+
+  }
+
+  useEffect(() => {
+    fetchPost()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <p>{posts.map(element => {
+        console.log(element.City)
+      })}</p>
+      <button onClick={fetchPost}>Click to get a new joke</button>
     </div>
   );
 }
